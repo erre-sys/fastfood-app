5a600bad973befcadd901d42f448b3b5e179f840
Eduardo Romero <38575968+wichoedu16@users.noreply.github.com>
2025-09-24 14:06:01 -0500

    chore(env): usa esquema Angular 18 (src/environments.ts)

---
+++ src/environments.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: '/api',
+  keycloak: {
+    url: 'http://100.43:8080',
+    realm: 'fastfood',
+    clientId: 'fastfood-web'
+  }
+};
---
+++ src/environments.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: '___API___',
+  keycloak: {
+    url: '___KC_URL___',
+    realm: '___KC_REALM___',
+    clientId: '___KC_CLIENT___'
+  }
+};
